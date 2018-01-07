885a6d299567a98bb57d4d699ee161c6c86c586b
Zsolt Benke <zsolt@decoding.io>
2018-01-07 22:20:05 +0100

    Alap keresés

--- CulinaryGuide/View Controllers/RestaurantsViewController.swift
+++ CulinaryGuide/View Controllers/RestaurantsViewController.swift
@@ -10,10 +10,16 @@ import Foundation
 import UIKit
 
 class RestaurantsViewController: UITableViewController {
+    let searchController = UISearchController(searchResultsController: nil)
     var restaurants = [Restaurant?]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchController.dimsBackgroundDuringPresentation = false
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.delegate = self
+        searchController.searchBar.delegate = self
+        self.navigationItem.searchController = searchController
 
         Restaurant.index { (loadedRestaurants) in
             self.restaurants = loadedRestaurants
@@ -58,6 +64,24 @@ class RestaurantsViewController: UITableViewController {
         let restaurant = restaurants[indexPath.row] as! Restaurant
         cell.textLabel!.text = restaurant.title
         return cell
+    }
+}
+
+extension RestaurantsViewController: UISearchControllerDelegate {
+
+}
+
+extension RestaurantsViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let searchToken = URLQueryToken.init(column: "search", value: searchBar.text!)
+        Restaurant.index(search: [searchToken]) { restaurants in
+            self.restaurants = restaurants
+            self.searchController.dismiss(animated: true)
+
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
     }
 }
 
