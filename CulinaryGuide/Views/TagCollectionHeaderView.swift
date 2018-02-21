757db10aa51e99b71ad5051494cc18016d513176
Zsolt Benke <zsolt@decoding.io>
2018-02-21 13:10:05 +0100

    Főscreen szekciócímek szövegszín beállítása

--- CulinaryGuide/Views/TagCollectionHeaderView.swift
+++ CulinaryGuide/Views/TagCollectionHeaderView.swift
@@ -3,4 +3,8 @@ import UIKit
 class TagCollectionHeaderView: UICollectionReusableView {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var separator: UIView!
+    
+    override func awakeFromNib() {
+        titleLabel.textColor = UIColor.BrandColor.facetText
+    }
 }
